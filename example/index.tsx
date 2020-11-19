1383f0354a6a24895cb63b3592d6a565b2df34a5
taylor-r-miller <64097840+taylor-r-miller@users.noreply.github.com>
2020-11-18 18:09:32 -0600

    included background image example

--- example/index.tsx
+++ example/index.tsx
@@ -1,17 +1,18 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import './index.css'
 import useWebp  from '../src/index';
+
 
 const App = () => {
 
-  const {supportsWebP} = useWebp()
-  const success = 'this browser supports webp'
-  const failure = 'this broswser does not support webp'
+  const { supportsWebP } = useWebp()
+
 
   return (
-    <div>
-      <h1>{supportsWebP ? success: failure }</h1>
+    <div className={supportsWebP ? 'bg-webp' : 'bg-jpg'}>
+      <h1>{`Background image is in ${supportsWebP ? 'webp' : 'jpg'} format` }</h1>
     </div>
   );
 };
